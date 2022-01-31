6320219b064986c23c4c74d5bf76b2df57b5ad33
SergGrane <94228017+SergGrane@users.noreply.github.com>
2022-01-31 12:51:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url ='????' + TOKEN)
+    bot.set_webhook(url ='https://telebot-first.herokuapp.com/' + TOKEN)
     return "Python Telegram Bot",200
 
 if __name__ == '__main__':
