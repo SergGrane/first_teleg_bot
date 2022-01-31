fcf19baf8a93afcc2c6c5725a6b5fbb509ec937e
SergGrane <94228017+SergGrane@users.noreply.github.com>
2022-01-31 20:01:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-#    bot.set_webhook(url='/' + TOKEN)
     bot.set_webhook(url ='https://telebot-first.herokuapp.com/' + TOKEN)
     return "Python Telegram Bot",200
 
