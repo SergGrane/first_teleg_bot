0d751554a4152b589b59fbf5304b7b1d07895782
SergGrane <94228017+SergGrane@users.noreply.github.com>
2022-01-31 17:03:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ def main():
     return "Python Telegram Bot",200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = int(os.environ.get('PORT',5000)))
+    app.run(host='0.0.0.0', port = int(os.environ.get('PORT',3000)))
 
